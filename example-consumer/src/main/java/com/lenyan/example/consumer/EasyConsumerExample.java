8b056b0d84b0e12cbe688d1c1c3454b85baee1be
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2025-02-23 21:31:36 +0800

    全局配置加载的开发

--- example-consumer/src/main/java/com/lenyan/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/lenyan/example/consumer/EasyConsumerExample.java
@@ -3,6 +3,8 @@ package com.lenyan.example.consumer;
 
 import com.lenyan.example.common.model.User;
 import com.lenyan.example.common.service.UserService;
+import com.lenyan.lenrpc.config.RpcConfig;
+import com.lenyan.lenrpc.utils.ConfigUtils;
 
 /**
  * 简易服务消费者示例
@@ -10,6 +12,8 @@ import com.lenyan.example.common.service.UserService;
 public class EasyConsumerExample {
 
     public static void main(String[] args) {
+        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        System.out.println(rpc);
         UserService userService = new UserServiceProxy();
         // todo 需要获取 UserService 的实现类对象
         User user = new User();
